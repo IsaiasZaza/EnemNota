7ead521c69a0b6067b196f8f9314c2fe84e06197
Isaias Gonçalves <isaias.legend@gmail.com>
2023-08-21 14:46:26 -0300

    fix

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 const somar = document.querySelector('#resultado');
 
-function calcularSoma(){
-   
-   //variaveis das notas
+function calcularSoma() {
+
+    //variaveis das notas
     let inputOne = parseFloat(document.querySelector(".inputOne").value);
     let inputTwo = parseFloat(document.querySelector(".inputTwo").value);
     let inputTres = parseFloat(document.querySelector(".inputTres").value);
     let inputQuatro = parseFloat(document.querySelector(".inputQuatro").value);
     let inputQuinto = parseFloat(document.querySelector(".inputQuinto").value);
-   
+
     //soma notas;
     let soma = inputOne + inputTwo + inputTres + inputQuatro + inputQuinto
     let somaTotal = soma / 5;
-   
+
     //Id Resultado
     let resultadoFinal = document.getElementById("resultado").textContent = " " + somaTotal;
-   
-    //Mudança de Cores conforme o Resultado
-    let trocarCor = document.querySelector('.cor-change');
-    const valor = 500;
+
 }
 
-
 const botao = document.querySelector('.botao');
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function () {
     let modal = document.querySelector('.modal');
     let fechar = document.querySelector('.fechar');
     modal.style.display = 'block';
@@ -31,16 +27,23 @@ botao.addEventListener("click", function(){
 
 const fechar = document.querySelector('.fechar')
 
-fechar.addEventListener("click", function(){
+fechar.addEventListener("click", function () {
     let modal = document.querySelector('.modal');
     modal.style.display = 'none';
 })
 
 
- /* treino teste if(soma >= 0 && soma <= 500){
-        trocarCor.style.backgroundColor = 'red';
-    }if(soma >= 500 && soma <= 800){
-        trocarCor.style.backgroundColor = 'blue';
-    }else{
-        trocarCor.style.backgroundColor = 'black';
-    }*/
+/*
+//Mudança de Cores conforme o Resultado
+   let trocarCor = document.querySelector('.cor-change');
+   const valor = 500;
+ 
+treino teste if(soma >= 0 && soma <= 500){
+       trocarCor.style.backgroundColor = 'red';
+   }if(soma >= 500 && soma <= 800){
+       trocarCor.style.backgroundColor = 'blue';
+   }else{
+       trocarCor.style.backgroundColor = 'black';
+   }*/
+
+   
